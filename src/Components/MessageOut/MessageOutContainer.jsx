8845e2936e6ed4d5b345c31a4a4dd228fe8f34d2
Jack Dawson <150480928+jacksoncdawson@users.noreply.github.com>
2024-02-17 15:07:23 -0800

    fixes

--- src/Components/MessageOut/MessageOutContainer.jsx
+++ src/Components/MessageOut/MessageOutContainer.jsx
@@ -4,45 +4,44 @@ import db from "../../firebase";
 import MessageOutView from "./MessageOutView";
 
 function MessageOutContainer() {
-  const [message, setMessage] = useState("");
+	const [message, setMessage] = useState("");
 
-  // Input Change Handler (Limit to 500 Characters)
-  const handleInputChange = (event) => {
-    const inputText = event.target.value;
-    if (inputText.length > 500) {
-      setMessage(inputText.slice(0, 500));
-    } else {
-      setMessage(inputText);
-    }
-  };
+	// Input Change Handler (Limit to 500 Characters)
+	const handleInputChange = (event) => {
+		const inputText = event.target.value;
+		if (inputText.length > 500) {
+			setMessage(inputText.slice(0, 500));
+		} else {
+			setMessage(inputText);
+		}
+	};
 
-  // Submit Handler (Add Message to Firestore)
-  const handleSubmit = async () => {
-    // Handle Empty Message
-    if (message.trim() === "") {
-      // alert("Cannot submit an empty message.");
-      return;
-    }
+	// Submit Handler (Add Message to Firestore)
+	const handleSubmit = async () => {
+		// Handle Empty Message
+		if (message.trim() === "") {
+			return;
+		}
 
-    // Handle Message Submission
-    try {
-      await addDoc(collection(db, "messages"), {
-        text: message,
-      });
-      alert("Message submitted!");
-    } catch (error) {
-      alert("Error submitting message: ", error);
-    }
-    setMessage("");
-  };
+		// Handle Message Submission
+		try {
+			await addDoc(collection(db, "messages"), {
+				text: message,
+			});
+			alert("Message submitted!");
+		} catch (error) {
+			alert("Error submitting message: ", error);
+		}
+		setMessage("");
+	};
 
-  return (
-    <MessageOutView
-      message={message}
-      handleInputChange={handleInputChange}
-      handleSubmit={handleSubmit}
-    />
-  );
+	return (
+		<MessageOutView
+			message={message}
+			handleInputChange={handleInputChange}
+			handleSubmit={handleSubmit}
+		/>
+	);
 }
 
 export default MessageOutContainer;
